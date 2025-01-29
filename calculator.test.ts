4e7a41b60c506d9a3ae257ee360c35a4e6de1289
Gaurav <gaurav.jogani@intermiles.com>
2025-01-29 11:30:00 +0530

    --handled Single Number addition--

--- calculator.test.ts
+++ calculator.test.ts
@@ -4,3 +4,7 @@ import { test, expect } from '@jest/globals';
 test('returns 0 for an empty string', () => {
   expect(add('')).toBe(0);
 });
+
+test('returns the number itself for a single number', () => {
+    expect(add('1')).toBe(1);
+});
